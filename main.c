6b4cef503bd056c496bea6431201628bf579bd18
swati155 <72041157+swati155@users.noreply.github.com>
2021-02-20 23:24:48 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int month;
+    printf("Enter month one\n");
+    scanf("%d",&month);
+    if(month==1)
+    {
+        printf("febraury\n");
+        printf("number of day is 28 n");
+    }
+    return 0;
+}
